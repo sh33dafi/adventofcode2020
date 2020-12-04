75974541e905ec95f112980ce7e64c9fc326f7be
Yannick Houbrix <sh33dafi@users.noreply.github.com>
2020-12-04 15:19:07 +0100

    prep day 5

---
+++ day5/main.ts
@@ -0,0 +1,10 @@
+import {fileToArray} from '../lib/read-file';
+
+console.time('read input');
+const input = fileToArray('./input.txt');
+console.timeLog('read input');
+console.time('solution 1');
+console.timeLog('solution 1',() => {});
+
+console.time('solution 2');
+console.timeLog('solution 2',() => {});
---
+++ day5/solution.spec.ts
@@ -0,0 +1,18 @@
+import {fileToArray} from '../lib/read-file';
+
+const puzInput = fileToArray('./day4/input.txt');
+const testInput = [];
+
+describe('day 5', () => {
+    describe('puzzle 1', () => {
+        it('should return correct value', () => {
+            fail('not impl');
+        });
+    });
+
+    describe('puzzle 2', () => {
+        it('should return correct value', () => {
+            fail('not impl');
+        });
+    });
+});

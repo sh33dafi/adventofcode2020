f200bdea3370fe63d39381a68ca380d9303cc7b4
Yannick Houbrix <sh33dafi@users.noreply.github.com>
2020-12-05 20:23:37 +0100

    Solution day5

--- day5/solution.spec.ts
+++ day5/solution.spec.ts
@@ -1,18 +1,74 @@
-import {fileToArray} from '../lib/read-file';
+import {fileToArrayNoEmptyLines} from '../lib/read-file';
+import {
+    calculateSeatInfo,
+    determineRange,
+    findMissingSeats,
+    getColumn,
+    getHighestSeatId,
+    getRow,
+    getSeatId
+} from './solution';
 
-const puzInput = fileToArray('./day4/input.txt');
-const testInput = [];
+const puzInput = fileToArrayNoEmptyLines('./day5/input.txt');
 
 describe('day 5', () => {
+
+    describe('getRow', () => {
+        it('should return the correct row', () => {
+            expect(getRow(['F','B', 'F', 'B', 'B', 'F', 'F'])).toEqual(44)
+        });
+    });
+
+    describe('determineRange for RL', () => {
+        it('should return the correct column range', () => {
+            expect(['R'].reduce(determineRange, [0, 7])).toEqual([4,7])
+            expect(['R','L'].reduce(determineRange, [0, 7])).toEqual([4,5])
+            expect(['R','L', 'R'].reduce(determineRange, [0, 7])).toEqual([5,5])
+        });
+    });
+
+    describe('getColumn', () => {
+        it('should return the correct column', () => {
+            expect(getColumn(['R','L','R'])).toEqual(5)
+        });
+    });
+
+    describe('Calculate seat info', () => {
+        it('should calculate the correct seatInfo', () => {
+            expect(calculateSeatInfo('FBFBBFFRLR'.split(''))).toEqual([44, 5, 357]);
+            expect(calculateSeatInfo('BFFFBBFRRR'.split(''))).toEqual([70, 7, 567]);
+            expect(calculateSeatInfo('FFFBBBFRRR'.split(''))).toEqual([14, 7, 119]);
+            expect(calculateSeatInfo('BBFFBBFRLL'.split(''))).toEqual([102, 4, 820]);
+        });
+    });
+
+    describe('getSeat', () => {
+        it('should return the correct seat', () => {
+            expect(getSeatId(44, 5)).toEqual(357)
+        });
+    });
+
+    describe('get highest seat', () => {
+        it('should return the highest seat', () => {
+            const codes = [
+                'FBFBBFFRLR',
+                'BFFFBBFRRR',
+                'FFFBBBFRRR',
+                'BBFFBBFRLL'
+            ];
+            expect(getHighestSeatId(codes)).toEqual(820);
+        });
+    });
+
     describe('puzzle 1', () => {
         it('should return correct value', () => {
-            fail('not impl');
+            expect(getHighestSeatId(puzInput)).toEqual(938);
         });
     });
 
     describe('puzzle 2', () => {
         it('should return correct value', () => {
-            fail('not impl');
+            expect(findMissingSeats(puzInput)).toEqual(696);
         });
     });
 });
---
+++ day5/solution.ts
@@ -0,0 +1,57 @@
+export const getRow = (input: Array<string>): number => {
+    return input.reduce(determineRange, [0, 127])[0];
+
+};
+
+export const determineRange = (acc, rowIndication) => {
+    const [si, ei] = acc;
+    const half = Math.ceil((ei - si) / 2);
+    if (rowIndication === 'L' || rowIndication === 'F') {
+        return [si, ei - half];
+    } else {
+        return [si + half, ei];
+    }
+};
+
+export const getColumn = (input: Array<string>): number => {
+    return input.reduce(determineRange, [0, 7])[0];
+};
+
+export const getSeatId = (row: number, column: number): number => {
+    return row * 8 + column;
+};
+
+export const calculateSeatInfo = (input: Array<string>): Array<number> => {
+    const row = getRow(input.slice(0, 7));
+    const column = getColumn(input.slice(Math.max(input.length - 3, 1)));
+    const seatId = getSeatId(row, column);
+    return [row, column, seatId];
+};
+
+export const getHighestSeatId = (input: Array<string>): number => {
+    return input
+        .map(inp => calculateSeatInfo(inp.split('')))
+        .map(info => info[2])
+        .reduce((acc, value) => {
+            let highest = acc;
+            if (value > highest) {
+                highest = value;
+            }
+            return highest;
+        }, 0);
+};
+
+export const findMissingSeats = (input: Array<string>): number => {
+    const calcSeats = input
+        .map(inp => calculateSeatInfo(inp.split('')))
+        .map(info => info[2])
+        .sort((a, b) =>a-b)
+    ;
+
+    for(let i =0; i < calcSeats.length - 1; i++) {
+        if (!((calcSeats[i] + 1) === calcSeats[i+1])) {
+            return calcSeats[i] + 1;
+        }
+    }
+    return 0;
+}
